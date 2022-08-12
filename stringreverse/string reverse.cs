58018abfa323cfe49438793834cb8d3a93d341ea
HP <HP@DESKTOP-S1VRHNO>
2022-08-12 00:43:21 -0700

    Add project files.

---
+++ stringreverse/string reverse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringreverse
+{
+     class string_reverse
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("enter the string");
+            string str = Console.ReadLine();
+            string[] words = str.Split(' ');
+       
+            for(int i=0; i< words.Length;i++)
+            { string word = words[i];
+                string rev = "";
+                for (int j = 0; j < word.Length;j++)
+                {
+                    rev = word[j] + rev;
+                }
+                Console.Write(rev +" ");
+
+            }
+        }
+    }
+}
